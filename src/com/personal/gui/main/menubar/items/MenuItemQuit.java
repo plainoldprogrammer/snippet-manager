6d8dcec985bd179a564b87fe90fcef86ef5b5cb0
Arturo Orozco <plainoldprogrammer@gmail.com>
2018-06-16 16:37:04 -0700

    implementing the menu item

--- src/com/personal/gui/main/menubar/items/MenuItemQuit.java
+++ src/com/personal/gui/main/menubar/items/MenuItemQuit.java
@@ -1,6 +1,8 @@
 package com.personal.gui.main.menubar.items;
 
 import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MenuItemQuit extends JMenuItem
 {
@@ -9,5 +11,13 @@ public class MenuItemQuit extends JMenuItem
         super();
 
         setText("Quit");
+
+        addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                System.exit(0);
+            }
+        });
     }
 }
