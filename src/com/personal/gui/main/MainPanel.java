fe5de8a90a2ae81fa1287c76ef307bdb683c0023
Arturo Orozco <plainoldprogrammer@gmail.com>
2018-06-16 17:39:47 -0700

    Creation of the categories and snippets panels

--- src/com/personal/gui/main/MainPanel.java
+++ src/com/personal/gui/main/MainPanel.java
@@ -2,7 +2,9 @@ package com.personal.gui.main;
 
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
-import com.personal.gui.main.TextEditor;
+import javax.swing.JList;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class MainPanel extends JPanel
 {
@@ -10,8 +12,48 @@ public class MainPanel extends JPanel
     {
         super();
 
+        JPanel categoriesOptionsPanel = new JPanel();
+        categoriesOptionsPanel.setLayout(new BorderLayout());
+        categoriesOptionsPanel.setBackground(Color.GRAY);
+        JButton addCategorieButton = new JButton("+");
+        JButton removeCategorieButton = new JButton("-");
+        categoriesOptionsPanel.add(addCategorieButton, BorderLayout.NORTH);
+        categoriesOptionsPanel.add(removeCategorieButton, BorderLayout.SOUTH);
+
+        JPanel panelCategories = new JPanel();
+        panelCategories.setLayout(new BorderLayout());
+        String[] listLanguagesData = {"C++", "Java", "JavaScript"};
+        JList<String> languagesList = new JList<String>(listLanguagesData);
+        languagesList.setBackground(Color.GRAY);
+        panelCategories.add(languagesList, BorderLayout.CENTER);
+        panelCategories.add(categoriesOptionsPanel, BorderLayout.SOUTH);
+
+
+        JPanel snippetsOptionPanel = new JPanel();
+        snippetsOptionPanel.setLayout(new BorderLayout());
+        snippetsOptionPanel.setBackground(Color.LIGHT_GRAY);
+        JButton addSnippetButton = new JButton("s");
+        JButton removeSnippetButton = new JButton("s");
+        snippetsOptionPanel.add(addSnippetButton, BorderLayout.NORTH);
+        snippetsOptionPanel.add(removeSnippetButton, BorderLayout.SOUTH);
+
+        JPanel panelSnippets = new JPanel();
+        panelSnippets.setLayout(new BorderLayout());
+        String[] listSnippetsData = {"Hello World In Java", "Loop in C++", "Print JavaScript log"};
+        JList<String> snippetsList = new JList<String>(listSnippetsData);
+        snippetsList.setBackground(Color.LIGHT_GRAY);
+        panelSnippets.add(snippetsList, BorderLayout.CENTER);
+        panelSnippets.add(snippetsOptionPanel, BorderLayout.SOUTH);
+
+
+        JPanel categoriesAndSnippetsPanel = new JPanel();
+        categoriesAndSnippetsPanel.setLayout(new BorderLayout());
+        categoriesAndSnippetsPanel.add(panelCategories, BorderLayout.WEST);
+        categoriesAndSnippetsPanel.add(panelSnippets, BorderLayout.EAST);
+
         TextEditor textEditor = new TextEditor();
         setLayout(new BorderLayout());
+        add(categoriesAndSnippetsPanel, BorderLayout.WEST);
         add(textEditor, BorderLayout.CENTER);
     }
 }
