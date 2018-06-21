5d6576dafbffc4ab640c9f811c3f47b3df02a5f9
Arturo Orozco <plainoldprogrammer@gmail.com>
2018-06-20 17:33:27 -0700

    access to DB and generate 'Category' and 'Snippet' objects from DB data

--- src/main/java/com/personal/db/JdbcSqliteConnection.java
+++ src/main/java/com/personal/db/JdbcSqliteConnection.java
@@ -7,6 +7,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
+import com.personal.util.Snippet;
+import com.personal.util.Category;
 
 public class JdbcSqliteConnection
 {
@@ -18,7 +22,6 @@ public class JdbcSqliteConnection
         {
             Class.forName("org.sqlite.JDBC");
             String dbURL = "jdbc:sqlite:src/main/resources/snippets.db";
-            String sqlQuery = "SELECT * FROM snippets";
 
             Connection connection = DriverManager.getConnection(dbURL);
 
@@ -28,13 +31,45 @@ public class JdbcSqliteConnection
                 logger.info(connection);
                 Statement statement = connection.createStatement();
                 logger.info(statement);
+
+                String sqlQuery = "SELECT DISTINCT category FROM snippets";
                 ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+                List<Category> listOfCategories = new ArrayList();
 
                 while (resultSet.next())
                 {
-                    logger.info(resultSet.getInt("id") + "\t" + resultSet.getString("category") + "\t" +
-                        resultSet.getString("title") + "\t");
+
+                    String categoryName = resultSet.getString("category");
+                    logger.info(categoryName);
+                    Category currentCategory = new Category(categoryName);
+
+                    String sqlQueryForSnippetsOfACategory = "SELECT title, snippet FROM snippets WHERE category = '" + categoryName + "'";
+                    logger.info(sqlQueryForSnippetsOfACategory);
+
+                    Statement statementForSnippets = connection.createStatement();
+                    ResultSet resultSetOfSnippets = statementForSnippets.executeQuery(sqlQueryForSnippetsOfACategory);
+
+                    while (resultSetOfSnippets.next())
+                    {
+                        logger.info(resultSetOfSnippets.getString("title"));
+
+                        String titleOfSnippet = resultSetOfSnippets.getString("title");
+                        String codeSnippet = resultSetOfSnippets.getString("snippet");
+
+                        Snippet currentSnippet = new Snippet(titleOfSnippet, codeSnippet);
+                        currentCategory.addSnippet(currentSnippet);
+                    }
+
+                    listOfCategories.add(currentCategory);
                 }
+
+                logger.info("Categories: " + listOfCategories.size());
+                logger.info("Snippet in category 1: " + listOfCategories.get(0).getListOfSnippets().size());
+                logger.info("Snippet in category 2: " + listOfCategories.get(1).getListOfSnippets().size());
+                logger.info("Snippet in category 3: " + listOfCategories.get(2).getListOfSnippets().size());
+                logger.info("Snippet in category 4: " + listOfCategories.get(3).getListOfSnippets().size());
+
             }
         }
         catch (ClassNotFoundException e)
